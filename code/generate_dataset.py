bdc7e58b4285a9cb037b145b4d7f29548b15287f
Juan D. Montoro-Pons <55840169+juanDmontoro@users.noreply.github.com>
2025-06-10 00:05:50 +0200

    Update generate_dataset.py

--- code/generate_dataset.py
+++ code/generate_dataset.py
@@ -29,7 +29,7 @@ y = df['streams']
 # ALTERNATIVAS PARA LA respuesta: track_popularity, weeks, top_ten (muy pocas obs.), peek_position (se puede hacer top ten), times_at_peek (pocas obs.), peek_streams
 
 
-variables_eliminar = ['id','track_name',
+variables_eliminar = ['track_name',
                      'streams','nTags',
                     'markets',
                     'day',
